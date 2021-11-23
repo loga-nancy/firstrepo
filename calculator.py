8d60760d72c9c70bea5529957fd6fa2352b69e18
gitdemo <nancybeece96@gmail.com>
2021-11-23 19:30:39 +0530

    Calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers."""
+  
+  a = int(input())
+    b = int(input())
+    
+    def cal(a,b):
+        x=a+b
+        y=a-b
+        z=a*b
+        return x,y,z
+    
+i,j,k=cal(a,b)
+print(i)
+print(j)
+print(k)
